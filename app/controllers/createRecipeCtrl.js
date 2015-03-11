def0588a899d1cbddf06de454d087cd4644d3cd9
jpeyatt <justinpeyatt@Justins-MacBook-Pro.local>
2015-03-10 23:17:03 -0400

    Recipe Pad

--- app/controllers/createRecipeCtrl.js
+++ app/controllers/createRecipeCtrl.js
@@ -5,4 +5,34 @@ RpApp.controller('CreateRecipeCtrl',[function () {
     vm.selected = undefined;
     vm.genres = ['African', 'American', 'Asian', 'Caribbean', 'European', 'Oceanian', 'Mexican', 'Chicken', 'Steak', 'Beef', 'Ham', 'Pasta'];
 
+    vm.ingredients = [];
+    vm.addIngredient = function () {
+        vm.ingredients.push({
+            measurement: vm.newIngredientMeasurement,
+            name: vm.newIngredientName
+        });
+        vm.newIngredientMeasurement = '';
+        vm.newIngredientName = '';
+    };
+    vm.deleteIngredient = function (index) {
+        vm.ingredients.splice(index, 1);
+    };
+
+    vm.directions = [];
+    vm.addDirection = function () {
+        vm.directions.push({
+            step: vm.newDirectionStep,
+            instructions: vm.newDirectionInstructions
+        });
+        vm.newDirectionStep = '';
+        vm.newDirectionInstructions = '';
+    };
+    vm.deleteDirection = function (index) {
+       vm.directions.splice(index);
+    };
+
+    vm.createRecipe = function () {
+
+    };
+
 }]);
